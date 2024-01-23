2ffcf5784172b3a30c1c5c407a3b2629e98639b2
unknown <you@example.com>
2024-01-23 22:00:59 +0800

    Loop created and changes in pseudocode

--- current_number_plus_previous_number.py
+++ current_number_plus_previous_number.py
@@ -3,6 +3,10 @@
 # Set previous_num to 0
 print('Printing current and previous number sum in a range(10)')
 previous_num = 0
-# Loop from 0 to 10
 
-# Set current_num to "i"
+# Loop from 0 to 10
+for i in range(10):
+    sum = previous_num + i
+    print('Current number is', i , 'Previous number is', previous_num , 'Sum:', sum)
+
+# Set previous_num to "i"
